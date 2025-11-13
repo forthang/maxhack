2d8d8b4c722ac83cd656edffe8a14b5735a40e91
forthang <therealskyvvalker@gmail.com>
2025-11-14 02:51:35 +0300

    debug max bridge

--- frontend/src/components/CourseGraph.tsx
+++ frontend/src/components/CourseGraph.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import {
   ReactFlowProvider,
 } from '@xyflow/react';
