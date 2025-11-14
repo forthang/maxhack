1edf2126a88385097995ec1145f6a0761fb63579
forthang <therealskyvvalker@gmail.com>
2025-11-14 13:08:02 +0300

    new branch

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .user import User, UserCreate, UserUpdate, ProfileOut
 from .schedule import ScheduleItem, ScheduleItemCreate, ScheduleItemUpdate, ScheduleItemOut
 from .event import Event, EventCreate, EventUpdate, EventOut
+from .university import University, UniversityCreate, UniversityOut
 # Add other schemas here
